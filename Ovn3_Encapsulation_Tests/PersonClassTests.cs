789c9165c1a523c1c9a48e1fb6fd2bc1a42000c8
Alexander Stauch <blacke4dawn@gmail.com>
2026-05-07 09:26:26 +0200

    added unit testing

---
+++ Ovn3_Encapsulation_Tests/PersonClassTests.cs
@@ -0,0 +1,28 @@
+﻿using Ovn3_Encapsulation.Classes;
+
+namespace Ovn3_Encapsulation_Tests;
+
+public class PersonClassTests
+{
+    [Fact]
+    public void Test_Construction()
+    {
+        var person = new Person("Mike", "Hansson", 30);
+
+        Assert.Equal("Mike", person.FirstName);
+        Assert.Equal("Hansson", person.Lastname);
+        Assert.Equal(30, person.Age);
+    }
+
+    [Fact]
+    public void Test_ToString_Override()
+    {
+        var person = new Person("Mike", "Hansson", 30);
+
+        Assert.Equal(
+            "Mike Hansson is 30 years old.",
+            $"{person}"
+        );
+    }
+
+}
