bfce352acf82c9b0e93fad41ca59ce6dbd5955fe
Alexander Stauch <blacke4dawn@gmail.com>
2026-05-07 10:28:37 +0200

    pre-emptive update of test cases

--- Ovn3_Encapsulation_Tests/PersonClassTests.cs
+++ Ovn3_Encapsulation_Tests/PersonClassTests.cs
@@ -7,20 +7,21 @@ public class PersonClassTests
     [Fact]
     public void Test_Construction()
     {
-        var person = new Person("Mike", "Hansson", 30);
+        var person = new Person("Mike", "Hansson", 30, 3000);
 
         Assert.Equal("Mike", person.FirstName);
         Assert.Equal("Hansson", person.Lastname);
         Assert.Equal(30, person.Age);
+        Assert.Equal(3000, person.Salary);
     }
 
     [Fact]
     public void Test_ToString_Override()
     {
-        var person = new Person("Mike", "Hansson", 30);
+        var person = new Person("Mike", "Hansson", 30, 3000);
 
         Assert.Equal(
-            "Mike Hansson is 30 years old.",
+            "Mike Hansson recieves 3000.00 dollars.",
             $"{person}"
         );
     }
@@ -28,13 +29,31 @@ public class PersonClassTests
     [Fact]
     public void Age_BelowEighteen_ThrowsArgumentException()
     {
-        Assert.Throws<ArgumentException>(() => new Person("Mike", "Hansson", 3));
+        Assert.Throws<ArgumentException>(() => new Person("Mike", "Hansson", 3, 3000));
     }
 
     [Fact]
     public void Age_SetBelowEighteen_ThrowsArgumentException()
     {
-        var person = new Person("Mike", "Hansson", 30);
+        var person = new Person("Mike", "Hansson", 30, 3000);
         Assert.Throws<ArgumentException>(() => person.Age = 3);
+    }
+
+    [Fact]
+    public void Older_GetsFull_Bonus()
+    {
+        var person = new Person("Mike", "Hansson", 30, 3000);
+
+        person.IncreaseSalary(10);
+        Assert.Equal(3300, person.Salary);
+    }
+
+    [Fact]
+    public void Younger_GetsHalf_Bonus()
+    {
+        var person = new Person("Maja", "Petterson", 22, 2200);
+
+        person.IncreaseSalary(10);
+        Assert.Equal(2310, person.Salary);
     }
 }
